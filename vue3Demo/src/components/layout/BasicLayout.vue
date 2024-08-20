a140a2d695d43fd20c595e6e6bad52e21b493fd8
foolvip <foolvip@163.com>
2024-08-20 20:03:00 +0800

    feat：路由修改

--- vue3Demo/src/components/layout/BasicLayout.vue
+++ vue3Demo/src/components/layout/BasicLayout.vue
@@ -13,13 +13,7 @@
         @expand="collapsed = false"
       >
         <n-menu
-          :collapsed="collapsed"
-          :collapsed-width="64"
-          :collapsed-icon-size="22"
           :options="menuOptions"
-          :render-label="renderMenuLabel"
-          :render-icon="false"
-          :expand-icon="expandIcon"
         />
       </n-layout-sider>
       <n-layout>
@@ -38,79 +32,125 @@
 
 <script lang="ts" setup>
 import { h, ref } from 'vue'
+import type { Component } from 'vue'
+import { RouterLink } from 'vue-router'
 import { NIcon } from 'naive-ui'
 import type { MenuOption } from 'naive-ui'
-// import { BookmarkOutline, CaretDownOutline } from '@vicons/ionicons5'
+import { BookmarkOutline, CaretDownOutline } from '@vicons/ionicons5'
+import {routes} from '@/router/index'
+console.log('router---routes-:', routes)
 
-const menuOptions: MenuOption[] = [
-  {
-    label: '且听风吟',
-    key: 'hear-the-wind-sing',
-    href: 'https://baike.baidu.com/item/%E4%B8%94%E5%90%AC%E9%A3%8E%E5%90%9F/3199'
-  },
-  {
-    label: '1973年的弹珠玩具',
-    key: 'pinball-1973',
-    disabled: true,
-    children: [
-      {
-        label: '鼠',
-        key: 'rat'
-      }
-    ]
-  },
-  {
-    label: '寻羊冒险记',
-    key: 'a-wild-sheep-chase',
-    disabled: true
-  },
-  {
-    label: '舞，舞，舞',
-    key: 'dance-dance-dance',
-    children: [
-      {
-        type: 'group',
-        label: '人物',
-        key: 'people',
-        children: [
+function renderIcon(icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon || BookmarkOutline) })
+}
+
+const getMenuOptions= (menus: MenuOption[]) => {
+  const menuOptions = menus.map(item => {
+    let newItem = {...item }
+    if (item.children && item.children.length === 1) {
+      newItem = {...item.children[0]}
+    }
+    const itemMenu = {
+      // label: item.meta ? item.meta.title : item.children ? item.children[0].meta.title : '',
+      label: () =>
+        h(
+          RouterLink,
           {
-            label: '叙事者',
-            key: 'narrator'
+            to: {
+              name: newItem.name,
+            },
           },
-          {
-            label: '羊男',
-            key: 'sheep-man'
-          }
-        ]
-      },
-      {
-        label: '饮品',
-        key: 'beverage',
-        children: [
-          {
-            label: '威士忌',
-            key: 'whisky',
-            href: 'https://baike.baidu.com/item/%E5%A8%81%E5%A3%AB%E5%BF%8C%E9%85%92/2959816?fromtitle=%E5%A8%81%E5%A3%AB%E5%BF%8C&fromid=573&fr=aladdin'
-          }
-        ]
-      },
-      {
-        label: '食物',
-        key: 'food',
-        children: [
-          {
-            label: '三明治',
-            key: 'sandwich'
-          }
-        ]
-      },
-      {
-        label: '过去增多，未来减少',
-        key: 'the-past-increases-the-future-recedes'
-      }
-    ]
-  }
-]
+          { default: () => newItem.meta ? newItem.meta.title : '' }
+        ),
+      key: newItem.name,
+      disabled:  newItem.meta ? newItem.meta.disabled : false,
+      icon: renderIcon(newItem.meta ? newItem.meta.icon : '')
+    }
+    if (newItem.children && newItem.children.length === 1) {
+      itemMenu.children = getMenuOptions(newItem.children)
+    }
+    if (newItem.children && newItem.children.length > 1) {
+      itemMenu.children = getMenuOptions(newItem.children)
+    }
+    console.log(itemMenu, 'itemMenu335')
+    return itemMenu
+  })
+
+  return menuOptions;
+}
+
+const menu = routes.slice(1)
+const menuOptions = getMenuOptions(menu)
+
+// const menuOptions: MenuOption[] = [
+//   {
+//     label: '且听风吟',
+//     key: 'hear-the-wind-sing',
+//     href: 'https://baike.baidu.com/item/%E4%B8%94%E5%90%AC%E9%A3%8E%E5%90%9F/3199'
+//   },
+//   {
+//     label: '1973年的弹珠玩具',
+//     key: 'pinball-1973',
+//     // disabled: true,
+//     children: [
+//       {
+//         label: '鼠',
+//         key: 'rat'
+//       }
+//     ]
+//   },
+//   {
+//     label: '寻羊冒险记',
+//     key: 'a-wild-sheep-chase',
+//     disabled: true
+//   },
+//   {
+//     label: '舞，舞，舞',
+//     key: 'dance-dance-dance',
+//     children: [
+//       {
+//         type: 'group',
+//         label: '人物',
+//         key: 'people',
+//         children: [
+//           {
+//             label: '叙事者',
+//             key: 'narrator'
+//           },
+//           {
+//             label: '羊男',
+//             key: 'sheep-man'
+//           }
+//         ]
+//       },
+//       {
+//         label: '饮品',
+//         key: 'beverage',
+//         children: [
+//           {
+//             label: '威士忌',
+//             key: 'whisky',
+//             href: 'https://baike.baidu.com/item/%E5%A8%81%E5%A3%AB%E5%BF%8C%E9%85%92/2959816?fromtitle=%E5%A8%81%E5%A3%AB%E5%BF%8C&fromid=573&fr=aladdin'
+//           }
+//         ]
+//       },
+//       {
+//         label: '食物',
+//         key: 'food',
+//         children: [
+//           {
+//             label: '三明治',
+//             key: 'sandwich'
+//           }
+//         ]
+//       },
+//       {
+//         label: '过去增多，未来减少',
+//         key: 'the-past-increases-the-future-recedes'
+//       }
+//     ]
+//   }
+// ]
 const collapsed = ref(true)
 
 const renderMenuLabel = (option: MenuOption) => {
@@ -123,15 +163,15 @@ const renderMenuLabel = (option: MenuOption) => {
   }
   return option.label as string
 }
-// const renderMenuIcon = (option: MenuOption) => {
-//   // 渲染图标占位符以保持缩进
-//   if (option.key === 'sheep-man')
-//     return true
-//   // 返回 falsy 值，不再渲染图标及占位符
-//   if (option.key === 'food')
-//     return null
-//   return h(NIcon, null, { default: () => h(BookmarkOutline) })
-// }
+const renderMenuIcon = (option: MenuOption) => {
+  // 渲染图标占位符以保持缩进
+  if (option.key === 'sheep-man')
+    return true
+  // 返回 falsy 值，不再渲染图标及占位符
+  if (option.key === 'food')
+    return null
+  return h(NIcon, null, { default: () => h(BookmarkOutline) })
+}
 
 const expandIcon = () => {
   return h(NIcon, null, { default: () => h(CaretDownOutline) })
--- vue3Demo/src/router/index.ts
+++ vue3Demo/src/router/index.ts
@@ -2,40 +2,66 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 import BasicLayout from '@/components/layout/BasicLayout.vue'
+import ProjectList from '@/views/project/List.vue'
+
+import { BookOutline, BriefcaseOutline } from '@vicons/ionicons5'
+
+export const routes = [
+  {
+    path: '/',
+    redirect: { name: 'worktable' }
+  },
+  {
+    name: 'worktable',
+    path: '/worktable',
+    component: BasicLayout,
+    children: [
+      {
+        name: 'worktableIndex',
+        path: '',
+        component:() => HomeView,
+        meta: {
+          title: '工作台',
+          icon: BookOutline
+        }
+      }
+    ],
+  },
+  {
+    path: '/project',
+    component: BasicLayout,
+    children: [
+      {
+        name: 'projectList',
+        path: '/list',
+        component:() => ProjectList,
+        meta: {
+          title: '项目列表',
+          icon: BriefcaseOutline
+        }
+      }
+    ],
+    meta: {
+      title: '项目',
+      icon: BriefcaseOutline
+    }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue'),
+    meta: {
+      title: '关于',
+    }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      redirect: { name: 'worktable' }
-    },
-    {
-      path: '/worktable',
-      component: BasicLayout,
-      children: [
-        {
-          path: '',
-          name: 'worktable',
-          component:() => HomeView,
-          meta: {
-            title: '工作台',
-          }
-        }
-      ],
-      meta: {
-        title: 'home',
-      },
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
