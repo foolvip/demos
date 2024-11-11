3dfa161ae8466533281af8eb79ae4ec8fa1c8928
foolvip <foolvip@163.com>
2024-11-11 19:15:04 +0800

    feat：打包配置

---
+++ vue3-vite-ts-storybook/src/components/Button/index.ts
@@ -0,0 +1,5 @@
+import { withInstall } from '../../utils'
+import Button from './Button.vue'
+
+export const SnailButton = withInstall(Button)
+export default SnailButton
--- vue3-vite-ts-storybook/vite.config.ts
+++ vue3-vite-ts-storybook/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      // 入口文件将包含可以由你的包的用户导入的导出
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "mthBusinessCom",
+      fileName: (format) => `index.${format}.js`,
+    },
+  }
 })
